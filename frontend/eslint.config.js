0efa5872158561215e9d5be5824e919aec5fab58
jarretphon <jarretphon@gmail.com>
2026-05-22 11:16:18 +0800

    Set up frontend structure

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,22 @@
-// eslint.config.js
-import js from "@eslint/js";
-import globals from "globals";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
-  js.configs.recommended,
-
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.es2021,
-      },
-    },
-
-    rules: {
-      indent: ["error", 2],
-      "linebreak-style": ["error", "unix"],
-      quotes: ["error", "double"],
-      semi: ["error", "always"],
-      "no-unused-vars": "warn",
+      globals: globals.browser,
     },
   },
-];
+])
